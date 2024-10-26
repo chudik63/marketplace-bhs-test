cf1310666c73e646436b47acb12799dfa0198395
Vitalya <vitamin11182005@gmail.com>
2024-10-26 09:39:18 +0300

    move server startup to internal/app

--- cmd/main.go
+++ cmd/main.go
@@ -2,15 +2,8 @@ package main
 
 import (
 	"log"
-	"marketplace-bhs-test/internal/auth"
-	"marketplace-bhs-test/internal/delivery/http"
-	"marketplace-bhs-test/internal/delivery/http/middleware"
+	"marketplace-bhs-test/internal/app"
 	"marketplace-bhs-test/internal/infrastructure"
-	"marketplace-bhs-test/internal/infrastructure/database"
-	"marketplace-bhs-test/internal/repository"
-	"marketplace-bhs-test/internal/service"
-
-	"github.com/gin-gonic/gin"
 )
 
 func main() {
@@ -19,29 +12,5 @@ func main() {
 		log.Fatalf("Failed to load configuration: %v", err)
 	}
 
-	db, err := database.NewPostgresDB(cfg)
-	if err != nil {
-		log.Fatalf("Failed to connect to DB: %v", err)
-	}
-
-	tokenManager, err := auth.NewManager(cfg.Auth.SecretKey)
-	if err != nil {
-		log.Fatalf("Failed to create token manager: %v", err)
-	}
-
-	userRepo := repository.NewUserRepository(db)
-	assetRepo := repository.NewAssetRepository(db, userRepo)
-
-	userService := service.NewUserService(userRepo, tokenManager, cfg.Auth.AccessTokenTTL, cfg.Auth.RefreshTokenTTL)
-	assetService := service.NewAssetService(assetRepo)
-
-	router := gin.Default()
-	router.Use(middleware.AuthMiddleware(tokenManager))
-
-	http.NewUserHandler(router, userService)
-	http.NewAssetHandler(router, assetService)
-
-	if err := router.Run(cfg.Server.Port); err != nil {
-		log.Fatalf("Failed to run server: %v", err)
-	}
+	app.Run(cfg)
 }
---
+++ internal/app/app.go
@@ -0,0 +1,42 @@
+package app
+
+import (
+	"log"
+	"marketplace-bhs-test/internal/auth"
+	"marketplace-bhs-test/internal/delivery/http"
+	"marketplace-bhs-test/internal/delivery/http/middleware"
+	"marketplace-bhs-test/internal/infrastructure"
+	"marketplace-bhs-test/internal/infrastructure/database"
+	"marketplace-bhs-test/internal/repository"
+	"marketplace-bhs-test/internal/service"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Run(cfg *infrastructure.Config) {
+	db, err := database.NewPostgresDB(cfg)
+	if err != nil {
+		log.Fatalf("Failed to connect to DB: %v", err)
+	}
+
+	tokenManager, err := auth.NewManager(cfg.Auth.SecretKey)
+	if err != nil {
+		log.Fatalf("Failed to create token manager: %v", err)
+	}
+
+	userRepo := repository.NewUserRepository(db)
+	assetRepo := repository.NewAssetRepository(db, userRepo)
+
+	userService := service.NewUserService(userRepo, tokenManager, cfg.Auth.AccessTokenTTL, cfg.Auth.RefreshTokenTTL)
+	assetService := service.NewAssetService(assetRepo)
+
+	router := gin.Default()
+	router.Use(middleware.AuthMiddleware(tokenManager))
+
+	http.NewUserHandler(router, userService)
+	http.NewAssetHandler(router, assetService)
+
+	if err := router.Run(cfg.Server.Port); err != nil {
+		log.Fatalf("Failed to run server: %v", err)
+	}
+}
